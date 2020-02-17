05cb0ab011dc59be2f5e0e606645b6da1cd229be
Lauren <lpehlivanian00@stuy.edu>
2020-02-16 21:32:39 -0800

    duplicated mr. dw testing case from work02, works

--- main.c
+++ main.c
@@ -16,34 +16,50 @@ int main() {
   edges = new_matrix(4, 4);
 
   // //octants 1 and 5
+  c.red = 0;
+  c.green = 255;
+  c.blue = 0;
   add_edge(edges, 0, 0 , 1, XRES-1, YRES-1, 1);
   add_edge(edges, 0, XRES-1, 1, YRES-1, 0, 1);
   add_edge(edges, 0, 0, 1, XRES-1, YRES / 2, 1);
   add_edge(edges, XRES-1, YRES-1, 1, 0, YRES / 2, 1);
+  draw_lines(edges,s,c);
+  free_matrix(edges);
 
   //octants 8 and 4
+  edges = new_matrix(4, 4);
   c.blue = 255;
   add_edge(edges, 0, YRES-1, 1, XRES-1, 0, 1);
   add_edge(edges, 0, YRES-1, 1, XRES-1, YRES/2, 1);
   add_edge(edges, XRES-1, 0, 1, 0, YRES/2, 1);
+  draw_lines(edges,s,c);
+  free_matrix(edges);
 
   //octants 2 and 6
+  edges = new_matrix(4, 4);
   c.red = 255;
   c.green = 0;
   c.blue = 0;
   add_edge(edges, 0, 0, 1, XRES/2, YRES-1, 1);
   add_edge(edges, XRES-1, YRES-1, 1, XRES/2, 0, 1);
+  draw_lines(edges,s,c);
+  free_matrix(edges);
 
   //octants 7 and 3
+  edges = new_matrix(4, 4);
   c.blue = 255;
   add_edge(edges, 0, YRES-1, 1, XRES/2, 0, 1);
   add_edge(edges, XRES-1, 0, 1, XRES/2, YRES-1, 1);
+  draw_lines(edges,s,c);
+  free_matrix(edges);
 
   //horizontal and vertical
+  edges = new_matrix(4, 4);
   c.blue = 0;
   c.green = 255;
   add_edge(edges, 0, YRES/2, 1, XRES-1, YRES/2, 1);
   add_edge(edges, XRES/2, 0, 1, XRES/2, YRES-1, 1);
+  draw_lines(edges,s,c);
 
   // print_matrix(edges);
   //
